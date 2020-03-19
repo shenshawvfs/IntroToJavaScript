5b2ad6e13cfc8ab88f01e69e351e80a06e9b9611
KIBBLEONE\Scott <scott@kibblegames.com>
2020-03-18 22:24:38 -0700

    Fixed up some template defaults ready for students

--- Scheduler/scripts/App.js
+++ Scheduler/scripts/App.js
@@ -1,30 +1,16 @@
 /**
- * App Singleton
- *
- * @copyright: (C) 2014-2017 Kibble Games Inc in cooperation with Vancouver Film School.  All Rights Reserved.
- * @author:    Scott Henshaw {@link mailto:shenshaw@vfs.com}
- * @version:   2.1.0
- *
- * @summary:   Framework Singleton Class to contain a web app
+ * @copyright: (C) 2020 Vancouver Film School.  All Rights Reserved.
+ * @author:    Your Name {@link mailto:ddXXname@vfs.com}
+ * @version:   1.0
  */
 'use strict';
 
-function iAmBothObsoleteAndModern( parameterOne, parameterTwo = 'Not Required') {
-
-    let someValue = parameterOne + parameterTwo;
-    return someValue
-}
-
-// export ... = this is something that can be imported to another module
-// ... default = this is the default thing to export can be imported without {}
-// ... class = this is an Object (not an instance of an Object), we can make more
+// export ... = something that can be imported to another module
+// ... default = the default thing to export can be imported without {}
+//      ... class = an Object definition (not an instance of an Object), we can make more
 export default class App {
 
-	constructor( device = 'web') {
-
-        // All we do is add a class to the body tag, one day some fancy css
-        // will do great things with desktop and mobile devices because of this
-        document.querySelector('body').classList.add( device );
+	constructor() {
 
         // Initialize any app variables or code we will need
 
--- Scheduler/scripts/index.js
+++ Scheduler/scripts/index.js
@@ -1,23 +1,19 @@
 /**
- * App Main - ENTRY POINT
- *
- * @copyright: (C) 2014-2019 Kibble Online Inc in cooperation with Vancouver Film School.  All Rights Reserved.
- * @author:    Scott Henshaw {@link mailto:shenshaw@vfs.com}
- * @version:   1.1.0
+ * @copyright: (C) 2020 Vancouver Film School.  All Rights Reserved.
+ * @author:    Your Name {@link mailto:ddXXname@vfs.com}
+ * @version:   1.0
  */
 'use strict';
 
 import App from './App.js';
 
 (function Main(){
+
     // Wait for the DOM to finish loading (we don't want to reference things that don't exist)
     document.addEventListener('DOMContentLoaded', event => {
 
-        // Find out what we are running on
-        const MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test( navigator.userAgent );
-
         // Init the app and run it
-        let app = new App( MOBILE );
+        let app = new App();
         app.run()
     })
 })()
